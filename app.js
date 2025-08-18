4e5b4e6ba4cd2596c51da54dd5979cbdda54f15c
Avinash Tiwari <avinash.tiwari1093@gmail.com>
2025-08-18 14:57:43 +0530

    react tutorial lernings

--- app.js
+++ app.js
@@ -3,6 +3,27 @@ import ReactDOM from "react-dom/client";
 
 //  React Element/ react.createElement --> Object==> Html Element when we rendor to dom
 
-const heading = React.createElement('h1', {id:'heading'}, "Namaste!");
-const root =  ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
+const heading = React.createElement("h1", { id: "heading" }, "Namaste!");
+console.log(heading);
+
+// H1 tag using JSX:
+const jsxHeading = <h1 id="heading">Namaste React Using JSX</h1>;
+
+console.log(jsxHeading);
+
+const number = 10000;
+
+// React Component
+const HeadingComponent = () => {
+  return (
+    <div>
+        {number}
+      <h1 id="heading">Hello I am React Component</h1>
+    </div>
+  );
+};
+
+// const HeadingComponent = () => (<h1 id="heading">Hello I am React Component</h1>);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent />);
