7a14be0fc78c8ebc660063009d49716f4a118b33
Avinash Tiwari <avinash.tiwari1093@gmail.com>
2025-08-18 18:49:33 +0530

    new react learnings , buld components and css changed are done

--- actualReact.js
+++ actualReact.js
@@ -1,60 +1,86 @@
 
-import React from "react";
-import ReactDOM from "react-dom/client";
+// import React from "react";
+// import ReactDOM from "react-dom/client";
 
 
-const parent = React.createElement('div',
-    { id: 'parent' },
-    React.createElement('div',
-        { id: 'child' },
-        [React.createElement('h1',
-            {}, "I am the H1 Tag!"
-        ),
-        React.createElement('h2',
-            {}, "I am the H2 Tag!"
-        )
-        ]
-    )
-)
+// const parent = React.createElement('div',
+//     { id: 'parent' },
+//     React.createElement('div',
+//         { id: 'child' },
+//         [React.createElement('h1',
+//             {}, "I am the H1 Tag!"
+//         ),
+//         React.createElement('h2',
+//             {}, "I am the H2 Tag!"
+//         )
+//         ]
+//     )
+// )
 
-const parent1 = React.createElement('div',
-    { id: 'parent' },
-    [React.createElement('div',
-        { id: 'child1' },
-        [React.createElement('h1',
-            {}, "I am the H1 Tag!"
-        ),
-        React.createElement('h2',
-            {}, "I am the H2 Tag!"
-        )
-        ]
-    ),
-    React.createElement('div',
-        { id: 'child2' },
-        [React.createElement('h3',
-            {}, "I am the H3 Tag!"
-        ),
-        React.createElement('h4',
-            {}, "I am the H4 Tag!"
-        )
-        ]
-    )
-    ]
-)
-
-
-const heading = React.createElement("h1", {
-    id: "heading",
-    class: 'h1-01',
-    xyz: 'abc'
-}, "Hello World from React!");
-console.log(parent1);
-const root = ReactDOM.createRoot(document.getElementById("root"));
+// const parent1 = React.createElement('div',
+//     { id: 'parent' },
+//     [React.createElement('div',
+//         { id: 'child1' },
+//         [React.createElement('h1',
+//             {}, "I am the H1 Tag!"
+//         ),
+//         React.createElement('h2',
+//             {}, "I am the H2 Tag!"
+//         )
+//         ]
+//     ),
+//     React.createElement('div',
+//         { id: 'child2' },
+//         [React.createElement('h3',
+//             {}, "I am the H3 Tag!"
+//         ),
+//         React.createElement('h4',
+//             {}, "I am the H4 Tag!"
+//         )
+//         ]
+//     )
+//     ]
+// )
 
 
-root.render(parent1);
+// const heading = React.createElement("h1", {
+//     id: "heading",
+//     class: 'h1-01',
+//     xyz: 'abc'
+// }, "Hello World from React!");
+// console.log(parent1);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// const heading = document.createElement("h1");
-// heading.innerHTML = "Hello World From JavaScript!"
-// const root = document.getElementById("root");
-// root.appendChild(heading)
+
+// root.render(parent1);
+
+// // const heading = document.createElement("h1");
+// // heading.innerHTML = "Hello World From JavaScript!"
+// // const root = document.getElementById("root");
+// // root.appendChild(heading)
+
+
+// //  React Element/ react.createElement --> Object==> Html Element when we rendor to dom
+
+// const heading = React.createElement("h1", { id: "heading" }, "Namaste!");
+// console.log(heading);
+
+// // H1 tag using JSX:
+// const jsxHeading = <h1 id="heading">Namaste React Using JSX</h1>;
+
+// const number = 10000;
+
+// // React Component
+// const HeadingComponent = () => {
+//   return (
+//     <div>
+//       {number}
+//       <h1 id="heading">Hello I am React Component</h1>
+//     </div>
+//   );
+// };
+
+// // const HeadingComponent = () => (<h1 id="heading">Hello I am React Component</h1>);
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<HeadingComponent />);
--- app.js
+++ app.js
@@ -1,29 +1,101 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//  React Element/ react.createElement --> Object==> Html Element when we rendor to dom
+/**
+ * Header
+ *  - Logo
+ *  - Menu/Nav Items
+ * Body
+ *  - Search
+ *  - Restauranr cintainer
+ *    - Restraurant card
+ *     - Image
+ *     Name of Res, Stars, Cuisine
+ * Footer
+ *  - Cpoyright
+ *  - Links
+ *  - Address
+ *  - Contact Information
+ */
 
-const heading = React.createElement("h1", { id: "heading" }, "Namaste!");
-console.log(heading);
-
-// H1 tag using JSX:
-const jsxHeading = <h1 id="heading">Namaste React Using JSX</h1>;
-
-console.log(jsxHeading);
-
-const number = 10000;
-
-// React Component
-const HeadingComponent = () => {
+const Body = () => {
   return (
-    <div>
-        {number}
-      <h1 id="heading">Hello I am React Component</h1>
+    <>
+      <div className="body">
+        <div className="search">Search</div>
+        <div className="res-container">
+          <RestraurantCard resName="Meghna Foods" cuisine="Biryani,North Indian" rating="4.4" eta="20"/>
+          <RestraurantCard resName="KFC" cuisine="Fast Food, American" rating="4.0" eta="30"/>
+        </div>
+      </div>
+    </>
+  );
+};
+
+const RestraurantCard = (props) => {
+  return (
+    <>
+      <div
+        className="res-card"
+        style={{
+          backgroundColor: "#f0f0f0",
+        }}
+      >
+        <img
+          className="res-logo"
+          src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/9/17/7c2b954f-7c5f-41e7-92cb-56a4369e4522_678582.jpg"
+        />
+        <h3>{props.resName}</h3>
+        <h4>{props.cuisine}</h4>
+        <h4>{props.rating} stars</h4>
+        <h4>{props.eta} minutes</h4>
+      </div>
+    </>
+  );
+};
+
+const Header = () => {
+  return (
+    <>
+      <div className="header">
+        <div className="logo-container">
+          <img
+            className="logo"
+            src="https://dynamic.brandcrowd.com/asset/logo/de2f1188-627e-4b67-a8c3-1a2fc17d783e/logo-search-grid-2x?logoTemplateVersion=2&v=638579027116130000"
+          />
+        </div>
+        <div className="nav-items">
+          <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Contact</li>
+            <li>Cart</li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span>© {new Date().getFullYear()} FoodCity. All rights reserved.</span>
     </div>
   );
 };
 
-// const HeadingComponent = () => (<h1 id="heading">Hello I am React Component</h1>);
+const AppLayout = () => {
+  return (
+    <>
+      <div className="app">
+        <Header />
+        <Body />
+        <Footer />
+      </div>
+    </>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
